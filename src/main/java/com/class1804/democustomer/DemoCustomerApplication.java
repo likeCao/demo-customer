2010621483439332a041e3e3f0cc7a555b93a0ef
Caolike1 <15632921766>
2020-03-15 19:26:28 +0800

    Initial commit

---
+++ src/main/java/com/class1804/democustomer/DemoCustomerApplication.java
@@ -0,0 +1,14 @@
+package com.class1804.democustomer;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCustomerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/class1804/democustomer/config/MyConfig.java
@@ -0,0 +1,20 @@
+package com.class1804.democustomer.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+//配置类
+@Configuration
+@EnableTransactionManagement
+@MapperScan(basePackages = {"com.class1804.democustomer.dao"})
+public class MyConfig extends WebMvcConfigurerAdapter {
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations(ResourceUtils.CLASSPATH_URL_PREFIX+"/static/");
+        super.addResourceHandlers(registry);
+    }
+}
